53c796546bfa11679d78f84ad674c7641111b45f
#emmyCode <emmyCode4495@gmail.com>
2020-06-11 01:01:54 +0100

    switched to postgresql

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'to_do_app',
+        'USER': 'postgres',
+        'PASSWORD': 'Emmysquare10',
+        'HOST': 'localhost',
     }
 }
 
